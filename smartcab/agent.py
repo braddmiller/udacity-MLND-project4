eb0ff6afb578570b9aa5cc5c3bebe732006a1426
Bradley Miller <brad@blacktagdesign.com>
2016-03-24 16:18:51 -0600

    feat: Create basic random action code without learning First section of this assignment, create a random driving cab.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,6 +11,7 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.action_table = [None, 'forward', 'left', 'right']
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -20,32 +21,32 @@ class LearningAgent(Agent):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
+        print self.next_waypoint
+        print inputs
         deadline = self.env.get_deadline(self)
-
         # TODO: Update state
         
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice(self.action_table)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
-
+        print reward
         # TODO: Learn policy based on state, action, reward
-
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        if self.env.done:
+            print t
 
 def run():
     """Run the agent for a finite number of trials."""
-
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
     e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=.0001)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=1)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
